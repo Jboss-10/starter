83c8136c9d8d1ebf34eba70ac1029ec236f1b49d
Jboss-10 <jonathanbuss08@gmail.com>
2026-04-17 20:55:25 +0100

    oatmeal

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -194,4 +194,16 @@ return {
       },
     },
   },
+
+  {
+    "dustinblackman/oatmeal.nvim",
+    cmd = { "Oatmeal" },
+    keys = {
+      { "<leader>om", mode = "n", desc = "Start Oatmeal session" },
+    },
+    opts = {
+      --backend = "ollama",
+      --model = "codellama:latest",
+    },
+  },
 }
---
+++ lua/plugins/plugins.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "dustinblackman/oatmeal.nvim",
+    cmd = { "Oatmeal" },
+    keys = {
+      { "<leader>om", mode = "n", desc = "Start Oatmeal session" },
+    },
+    opts = {
+      backend = "ollama",
+    },
+  },
+}
